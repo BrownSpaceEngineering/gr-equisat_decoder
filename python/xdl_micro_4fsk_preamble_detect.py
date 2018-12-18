ee7afc8161806e720893761002e696a411b9caed
Mckenna Cisler <mckennacisler@gmail.com>
2018-12-18 03:07:28 -0500

    Start gr-satellites flowgraph; fix subtle bugs

--- python/xdl_micro_4fsk_preamble_detect.py
+++ python/xdl_micro_4fsk_preamble_detect.py
@@ -55,16 +55,23 @@ class xdl_micro_4fsk_preamble_detect(gr.basic_block):
 
     # largest possible/smallest acceptable preamble length (for starting to parse packet)
     MAX_PREAMBLE_LEN = 185  # symbols
-    MIN_PREAMBLE_LEN = 94  # symbols; must be mult of 4
+    DEF_MIN_PREAMBLE_LEN = 96  # symbols; must be mult of 4
 
     # maximum number of blocks to read and package after a preamble
     DEF_MAX_NUM_BLOCKS = 150
 
     # packet/preamble detection constants
     # percentage of first symbol in pair that second must be within to be "the same"
-    DEF_SYM_SIMILARITY_THRESH = 0.33
+    DEF_MAX_SYMBOL_RATIO = 0.33
 
-    def __init__(self, num_blocks=DEF_MAX_NUM_BLOCKS, max_symbol_ratio=DEF_SYM_SIMILARITY_THRESH):
+    # the maximum buffer to keep unconsumed in case preamble is present
+    # should be larger than MAX_PREAMBLE_SIZE or we'll miss preambles
+    MAX_UNCONSUMED_SIZE = 2 * MAX_PREAMBLE_LEN
+
+    # normalized threshold for cutoff between +/-3 and +/-1 symbols (where 1.0 is the +3 symbol)
+    SYM_SEPERATION_THRESH = 0.667
+
+    def __init__(self, num_blocks=DEF_MAX_NUM_BLOCKS, max_symbol_ratio=DEF_MAX_SYMBOL_RATIO, min_preamble_len=DEF_MIN_PREAMBLE_LEN):
         gr.basic_block.__init__(self,
             name="xdl_micro_4fsk_preamble_detect",
             in_sig=[np.float32],
@@ -74,10 +81,7 @@ class xdl_micro_4fsk_preamble_detect(gr.basic_block):
 
         self.num_blocks = num_blocks
         self.max_symbol_ratio = max_symbol_ratio
-
-        # set enough history to be large enough to store a full premable
-        self.NUM_OLD_DATA = self.MAX_PREAMBLE_LEN
-        # self.set_history(self.NUM_OLD_DATA)
+        self.min_preamble_len = min_preamble_len
 
         # the current state in the sequence of reading a packet
         self.state = self.ST_WAIT_FOR_PREAMBLE
@@ -108,13 +112,12 @@ class xdl_micro_4fsk_preamble_detect(gr.basic_block):
         # we've already read, plus possibly more if we didn't consume them,
         # but the rest are new (see set_history above)
         inpt = input_items[0]
-        new_inpt = inpt #inpt[self.NUM_OLD_DATA-1:]  # shortcut to new data; image of array
 
         # finite state machine for different sections of packet
         if self.state is self.ST_WAIT_FOR_PREAMBLE:
             # if searching, check for start of preamble in the HISTORY and
             # set up for start of packet if found
-            found, start, end, high, low = self.check_for_preamble(inpt)
+            found, start, end, high, low = self.check_for_preamble(inpt, self.min_preamble_len, self.max_symbol_ratio)
 
             # if we found a preamble sequence, but it took the whole buffer
             # (i.e. the last preamble sequence was within one sequence of the end)
@@ -124,43 +127,40 @@ class xdl_micro_4fsk_preamble_detect(gr.basic_block):
                 # setup packet state
                 self.high = high
                 self.low = low
-
-                # consume only the NEW part of the preamble
-                self.consume(0, end)
+                # consume the complete preamble
+                self.consume(0, end-1)
                 # now, try and start reading the frame sync
                 self.state = self.ST_IN_FRAME_SYNC
 
-            # wait for more data if no preamble was found (don't consume anything)
+            elif len(inpt) > self.MAX_UNCONSUMED_SIZE:
+                # generally, if no preamble was found, wait for more data (don't consume anything)
+                # HOWEVER, we don't want that buffer to get too big, so consume anything past the
+                # section where a preamble could be
+                self.consume(0, len(inpt) - self.MAX_PREAMBLE_LEN)
 
             return 0
 
         elif self.state == self.ST_IN_FRAME_SYNC:
             # check if the new data is enough to complete the frame sync
-            frame_sync_remaining = self.FRAME_SYNC_LEN - self.frame_sync_syms_read
-            if len(new_inpt) >= frame_sync_remaining:
+            if len(inpt) >= self.FRAME_SYNC_LEN:
                 # move onto blocks, starting past end of frame sync
                 self.state = self.ST_IN_BLOCKS
-                # consume the (rest of) the frame sync
-                self.consume(0, frame_sync_remaining)
-            else:
-                # state is still ST_IN_FRAME_SYNC, just note we read some more
-                self.frame_sync_syms_read += len(new_inpt)
-                # consume the frame sync that we've read
-                self.consume(0, len(new_inpt))
+                # consume the frame sync
+                self.consume(0, self.FRAME_SYNC_LEN)
 
             return 0
 
         elif self.state == self.ST_IN_BLOCKS:
             # copy the input into a buffer to transmit later, not consuming data that overflows that buffer
             blocks_buf_rem = len(self.blocks_buf) - self.blocks_buf_i
-            transferred = min(blocks_buf_rem, len(new_inpt))
-            self.blocks_buf[self.blocks_buf_i:self.blocks_buf_i+transferred] = new_inpt[:transferred]
+            transferred = min(blocks_buf_rem, len(inpt))
+            self.blocks_buf[self.blocks_buf_i:self.blocks_buf_i+transferred] = inpt[:transferred]
             self.blocks_buf_i += transferred
             self.consume(0, transferred)
 
             if self.blocks_buf_i >= len(self.blocks_buf): # should only equal
                 # now that we have a full buffer, convert to symbols
-                syms = self.get_symbols(self.blocks_buf, high=self.high, low=self.low)
+                syms = self.get_symbols(self.blocks_buf, high=self.high, low=self.low, sym_seperation_thresh=self.SYM_SEPERATION_THRESH)
 
                 # send message with symbols and reset state
                 syms_arr = array.array('B', syms)
@@ -174,8 +174,8 @@ class xdl_micro_4fsk_preamble_detect(gr.basic_block):
             assert False
 
     @staticmethod
-    def check_for_preamble(inpt, min_preamble_len=MIN_PREAMBLE_LEN,
-                           sym_sim_thresh=DEF_SYM_SIMILARITY_THRESH):
+    def check_for_preamble(inpt, min_preamble_len=DEF_MIN_PREAMBLE_LEN,
+                           sym_sim_thresh=DEF_MAX_SYMBOL_RATIO):
         """ Checks for a run of sets of 4 adjacent symbols resembling
         a preamble "cycle", i.e. -2 -2 +2 +2. Returns True if such a
         run of min_preamble_len symbols exists (min_preamble_len/4 such cycles).
@@ -248,7 +248,7 @@ class xdl_micro_4fsk_preamble_detect(gr.basic_block):
         return pair1_close and pair2_close and pair1_avg < 0 and pair2_avg > 0
 
     @staticmethod
-    def get_symbols(inpt, high, low):
+    def get_symbols(inpt, high, low, sym_seperation_thresh=SYM_SEPERATION_THRESH):
         """ Converts raw input values in a numpy array into numerical base-4 symbols,
         based on the given high value corresponding to +2 deviation and low to -2
         """
@@ -256,11 +256,11 @@ class xdl_micro_4fsk_preamble_detect(gr.basic_block):
         # and then threshold
         syms = 2 * (inpt - low) / float(high - low) - 1
         for i in range(len(syms)):
-            if syms[i] >= 0.75:
+            if syms[i] >= sym_seperation_thresh:
                 syms[i] = 1
             elif syms[i] >= 0.0:
                 syms[i] = 0
-            elif syms[i] > -0.75:
+            elif syms[i] > -sym_seperation_thresh:
                 syms[i] = 2
             else:
                 syms[i] = 3
